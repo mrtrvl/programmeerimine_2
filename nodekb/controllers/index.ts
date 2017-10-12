57e78546562bebdf420169873e5cce2b296aed1a
martti <mrt@mrt.ee>
2017-10-12 21:13:58 +0300

    delete post, error page

--- nodekb/controllers/index.ts
+++ nodekb/controllers/index.ts
@@ -14,15 +14,14 @@ router.get('/', (req: any, res: any) => {
     res.render('pages/index');
 });
 
-
+// Kõik postitused
 router.get('/posts', (req: any, res: any) => {
     Post.find({}, (err: any, posts: any) => {
         if(err) {
             console.log(err);
+            res.redirect('/error')
         } else {
             res.locals.posts = posts;
-            //console.log(posts);
-            //res.json(posts);
             res.render('pages/posts')
         }
     });
@@ -35,7 +34,6 @@ router.get('/posts/add', (req: any, res: any) => {
 
 //Postituse lisamine
 router.post('/posts/add', (req: any, res: any) => {
-    console.log(req.body);
     let newPost = new Post ({
         title: req.body.title,
         author: req.body.author,
@@ -45,7 +43,7 @@ router.post('/posts/add', (req: any, res: any) => {
     newPost.save((err: any) => {
         if (err) {
             console.log(err);
-            res.redirect('/posts/add');
+            res.redirect('/error');
         } else {
             res.redirect('/posts');
         }
@@ -58,7 +56,7 @@ router.get('/post/:id', (req: any, res: any) => {
     Post.findOne({_id: postId}).exec((err: any, post: any) => {
         if (err) {
             console.log(err);
-            res.redirect('/posts');
+            res.redirect('/error');
         } else {
             res.locals.post = post;
             res.render('pages/single-post');
@@ -72,7 +70,7 @@ router.get('/post/:id/edit', (req: any, res: any) => {
     Post.findOne({_id: postId}).exec((err: any, post: any) => {
         if (err) {
             console.log(err);
-            res.redirect('/posts');
+            res.redirect('/error');
         } else {
             res.locals.post = post;
             res.render('pages/edit-post');
@@ -82,7 +80,6 @@ router.get('/post/:id/edit', (req: any, res: any) => {
 
 //Postituse muutmine
 router.post('/post/:id/edit', (req: any, res: any) => {
-    console.log(req.body);
     let post = {
         title: req.body.title,
         author: req.body.author,
@@ -93,7 +90,7 @@ router.post('/post/:id/edit', (req: any, res: any) => {
     Post.update(query, post, (err:any) => {
         if (err) {
             console.log(err);
-            res.redirect('/post/' + req.params.id + "/edit");
+            res.redirect('/post/' + req.params.id + '/edit');
         } else {
             res.redirect('/post/' + req.params.id);
         }  
@@ -105,13 +102,12 @@ router.post('/post/:id/edit', (req: any, res: any) => {
 // Üksiku postituse kustutamine
 router.get('/post/:id/delete', (req: any, res: any) => {
     let postId = req.params.id;
-    Post.findOne({_id: postId}).exec((err: any, post: any) => {
+    Post.findByIdAndRemove({_id: postId}, (err: any) => {
         if (err) {
             console.log(err);
-            res.redirect('/posts');
+            res.redirect('/error');
         } else {
-            res.locals.post = post;
-            res.render('pages/single-post');
+            res.redirect('/posts');
         }
     });
 });
