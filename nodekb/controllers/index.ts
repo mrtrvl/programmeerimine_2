178edaa8a762bb35be563bd9f79be294547e0e45
martti <mrt@mrt.ee>
2017-10-12 16:34:41 +0300

    Add add-post view and controller

--- nodekb/controllers/index.ts
+++ nodekb/controllers/index.ts
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+let bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded 
+router.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json 
+router.use(bodyParser.json());
 
 const Post = require('../models/post');
 
@@ -17,6 +24,28 @@ router.get('/posts', (req: any, res: any) => {
             //console.log(posts);
             //res.json(posts);
             res.render('pages/posts')
+        }
+    });
+});
+
+router.get('/posts/add', (req: any, res: any) => {
+    res.render('pages/add-post')
+});
+
+router.post('/posts/add', (req: any, res: any) => {
+    console.log(req.body);
+    let newPost = new Post ({
+        title: req.body.title,
+        author: req.body.author,
+        content: req.body.content
+    });
+
+    newPost.save((err: any) => {
+        if (err) {
+            console.log(err);
+            res.redirect('/posts');
+        } else {
+            res.redirect('/posts');
         }
     });
 });
